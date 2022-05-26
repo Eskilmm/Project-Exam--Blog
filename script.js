83959c8d387367e3c16f39b04e2fe480bbd49037
Eskilmm <eskilmaroy@gmail.com>
2022-05-26 14:55:37 +0200

    e

--- script.js
+++ script.js
@@ -41,4 +41,21 @@ window.addEventListener('resize', () => {
     }, 400);
 });
 
-latest-row
+const productContainers= [...document.querySelectorAll('product-container')];
+
+const nxtBtn = [...document.querySelectorAll('nxt-btn')];
+const preBtn = [...document.querySelectorAll('pre-btn')];
+
+productContainers.forEach((item, i)=>{
+    let containerDimensions= item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () =>{
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () =>{
+        item.scrollLeft -= containerWidth;
+    })
+})
+
